ee0e52c5086ac6e7e110b8fbb7bb13c4e454b4ff
Sam Ragucci <samr742@gmail.com>
2026-02-27 22:14:48 -0800

    install (but not use) Jodit editor
    may come back to this and make it work

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+// import "jodit/build/jodit.min.css"; // need to install jodit for this to work
 import "./globals.css";
 import Navbar from "@/components/general/navbar/Navbar";
 
