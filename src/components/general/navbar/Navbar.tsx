46058a9e1bec4317f4b343569615431e5a91fb93
Sam Ragucci <samr742@gmail.com>
2026-03-07 11:52:18 -0800

    Hook up Login and Logout, show Write only when logged in

--- src/components/general/navbar/Navbar.tsx
+++ src/components/general/navbar/Navbar.tsx
@@ -5,6 +5,7 @@ import { LuMenu, LuNotebookPen, LuSearch, LuX } from "react-icons/lu";
 import MobileNav from "./MobileNav";
 import { useState } from "react";
 import { useModalStore } from "@/store/useModalStore";
+import { authClient } from "@/lib/auth-client";
 
 export const navLinks = [
   {url: "/", label: "Home"},
@@ -15,6 +16,12 @@ export const navLinks = [
 export default function Navbar() {
   const {openSignIn, openSearch} = useModalStore();
   const [menuOpen, setMenuOpen] = useState(false);
+  const {data:session, isPending} = authClient.useSession();
+
+  const handleLogout = async () => {
+    await authClient.signOut();
+  }
+
   function handleMenu() {
     setMenuOpen(!menuOpen);
   }
@@ -28,20 +35,32 @@ export default function Navbar() {
             <LuSearch size={20} />
             <span className="hidden md:block">Search</span>
           </li>
-          <li>
-            <Link href="/write" className="cursor-pointer flex items-center gap-1">
-              <LuNotebookPen size={20} />
-              <span className="hidden md:block">Write</span>
-            </Link>
-          </li>
+          {session && (
+            <li>
+              <Link href="/write" className="cursor-pointer flex items-center gap-1">
+                <LuNotebookPen size={20} />
+                <span className="hidden md:block">Write</span>
+              </Link>
+            </li>
+          )}
           {navLinks.map(({url, label}) => (
             <li key={url} className="hidden md:block hover:text-gray-200">
               <Link href={url}>{label}</Link>
             </li>
           ))}
+          {!isPending && (
+            <>
+            {session ? (
+          <li onClick={handleLogout} className="bg-primary text-gray-200 px-3 lg:px-5 py-2 rounded-full cursor-pointer">
+            Logout
+          </li>
+            ) : (
           <li onClick={openSignIn} className="bg-primary text-gray-200 px-3 lg:px-5 py-2 rounded-full cursor-pointer">
             Login
           </li>
+            )}
+            </>
+          )}
           <li className="cursor-pointer md:hidden z-80" onClick={handleMenu}>
             {menuOpen ? <LuX size={25}/> : <LuMenu size={25}/>}
           </li>
@@ -51,3 +70,7 @@ export default function Navbar() {
     </nav>
   )
 }
+
+/*
+
+*/
