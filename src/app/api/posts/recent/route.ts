adc7c5f9ed3585921169c8ca8853a567f74d43c0
Sam Ragucci <samr742@gmail.com>
2026-03-14 10:26:06 -0700

    first cut getting recent posts

---
+++ src/app/api/posts/recent/route.ts
@@ -0,0 +1,27 @@
+import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const posts = await prisma.post.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+      select:{
+        id: true,
+        title: true,
+        slug: true,
+        coverImageURL: true,
+        createdAt: true,
+      },
+      take: 6
+    });
+
+    return NextResponse.json({ posts }, {status: 200});
+
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to fetch recent posts, error: " + error }, 
+      { status: 500 });
+  }
+}
