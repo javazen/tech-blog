d3102978a0a51b92e7cdbc0a9e892cb6ae9e6251
Sam Ragucci <samr742@gmail.com>
2026-03-15 11:55:26 -0700

    get posts from server

--- src/components/home/RecentPosts.tsx
+++ src/components/home/RecentPosts.tsx
@@ -1,26 +1,37 @@
+import { Post } from "@/types/post";
 import Image from "next/image";
 import Link from "next/link";
 
-export const posts = [
-  {
-    id: 1,
-    title: "How to create a blog",
-    excerpt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.",
-    date: "May 31, 2023",
-    slug: "how-to-create-a-blog",
-    image: "/images/p1.png",
-  },
-  {
-    id: 2,
-    title: "Dark Mode Done Right",
-    excerpt: "Much longer bunch of ridiculous text to fill up this space with some overflow which will be hidden by the line clamp feature - that is to say, everything below the nth line will be replaced by an ellipsis!",
-    date: "May 2, 2023",
-    slug: "dark-mode-done-right",
-    image: "/images/p2.png",
-  },
-];
+// export const posts = [
+//   {
+//     id: 1,
+//     title: "How to create a blog",
+//     excerpt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.",
+//     date: "May 31, 2023",
+//     slug: "how-to-create-a-blog",
+//     image: "/images/p1.png",
+//   },
+//   {
+//     id: 2,
+//     title: "Dark Mode Done Right",
+//     excerpt: "Much longer bunch of ridiculous text to fill up this space with some overflow which will be hidden by the line clamp feature - that is to say, everything below the nth line will be replaced by an ellipsis!",
+//     date: "May 2, 2023",
+//     slug: "dark-mode-done-right",
+//     image: "/images/p2.png",
+//   },
+// ];
 
-export default function RecentPosts() {
+export default async function RecentPosts() { 
+  const res = await fetch(
+    `${process.env.BASE_URL}/api/posts/recent`, 
+    {cache: "no-store"});
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch recent posts");
+  }
+
+  const { posts }:{ posts:Post[] } = await res.json();
+
   return (
     <div className='space-y-2 mb-10'>
       <h2 className='text-white text-xl sm:text-2xl md:text-3xl font-semibold'>
@@ -34,16 +45,28 @@ export default function RecentPosts() {
             bg-[#0B0B0B]border border-white/10 transition-all duration-300
             hover:-translate-y-1 hover:border-white/20">
               {/* image */}
-              <div className="relative h-48 w-full overflow-hidden">
-                <Image src={post.image} alt={post.title}
-                  className="object-cover transition-transform duration-500 group-hover:scale-105" fill />
-                
-                <div className="absolute inset-0 bg-black/30"/> {/* overlay */}
-              </div>
+              {post.coverImageURL && (
+                <div className="relative h-48 w-full overflow-hidden">
+                  <Image src={post.coverImageURL} alt={post.title}
+                    className="object-cover transition-transform duration-500 group-hover:scale-105" fill />
+
+                  <div className="absolute inset-0 bg-black/30" /> {/* overlay */}
+                </div>
+
+              )}
               {/* content */}
               <div className="p-5 space-y-3">
                 <time>
-                  <span className="text-xs text-gray-400">{post.date}</span>
+                  <span className="text-xs text-gray-400">
+                    {new Date(post.createdAt).toLocaleDateString(
+                      "en-GB",
+                      {
+                        day: "numeric",
+                        month: "long",
+                        year: "numeric",
+                      }
+                    )}
+                  </span>
                 </time>
                 <h3 className="text-semibold text-lg text-white leading-snug
                 group-hover:text-indigo-400 transition-colors">
